b5cad66857f190adbfb88918d9c9ee4d79024785
PradeepKumar1006 <162351701+PradeepKumar1006@users.noreply.github.com>
2026-07-15 14:57:33 +0530

    Time: 511 ms (98.5%), Space: 0B (100%) - LeetHub

---
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -0,0 +1,15 @@
+SELECT
+    ROUND(
+        COUNT(DISTINCT a1.player_id) * 1.0 /
+        (SELECT COUNT(DISTINCT player_id) FROM Activity),
+        2
+    ) AS fraction
+FROM Activity a1
+JOIN Activity a2
+ON a1.player_id = a2.player_id
+AND a2.event_date = DATE_ADD(a1.event_date, INTERVAL 1 DAY)
+WHERE (a1.player_id, a1.event_date) IN (
+    SELECT player_id, MIN(event_date)
+    FROM Activity
+    GROUP BY player_id
+);
