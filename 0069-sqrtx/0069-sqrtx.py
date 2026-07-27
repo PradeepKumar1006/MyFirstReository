04d2be22ef938e6fed37231a7c8b9d45a56be8ff
PradeepKumar1006 <162351701+PradeepKumar1006@users.noreply.github.com>
2026-07-27 23:32:49 +0530

    Time: 0 ms (100%), Space: 19.1 MB (87.51%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,14 +1,12 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x < 2:
-            return x
-        l ,r = 0,x
-        while l <=r :
+        l , r = 0,x
+        while l <= r:
             m = l + (r-l)//2
             if m*m == x:
                 return m
             elif m*m < x:
-                l = m+1
+                l = m + 1
             else:
                 r = m -1
         return r
