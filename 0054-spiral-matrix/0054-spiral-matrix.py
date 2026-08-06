664520ce6384c81f6a652151d742ad6f7718469d
PradeepKumar1006 <162351701+PradeepKumar1006@users.noreply.github.com>
2026-08-06 11:31:10 +0530

    Time: 0 ms (100%), Space: 19.3 MB (33.88%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -4,8 +4,8 @@ class Solution:
         top = 0
         buttom = len(nums)-1
         left = 0
-        right = len(nums[0])-1
-        while left <= right and top <= buttom:
+        right = len(nums[0]) - 1
+        while top <= buttom and left <= right:
             for i in range(left,right+1):
                 res.append(nums[top][i])
             top += 1
@@ -13,12 +13,11 @@ class Solution:
                 res.append(nums[i][right])
             right -= 1
             if top <= buttom:
-                for i in range(right,left - 1,-1):
+                for i in range(right,left-1,-1):
                     res.append(nums[buttom][i])
                 buttom -= 1
-            if left <= right:
-                for i in range(buttom, top - 1, -1):
+            if left<= right:
+                for i in range(buttom,top -1 ,-1):
                     res.append(nums[i][left])
                 left += 1
-
         return res
