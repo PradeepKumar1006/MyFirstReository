043e8547b99d9d2215bc4fa42c43d73c2e17ab63
PradeepKumar1006 <162351701+PradeepKumar1006@users.noreply.github.com>
2026-08-06 11:44:15 +0530

    Time: 0 ms (100%), Space: 19.3 MB (28.38%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.py
+++ 0048-rotate-image/0048-rotate-image.py
@@ -1,9 +1,9 @@
 class Solution:
     def rotate(self, nums: List[List[int]]) -> None:
         n = len(nums)
-        
+        m = len(nums[0])
         for i in range(n):
-            for j in range(i+1,n):
-                nums[i][j], nums[j][i] = nums[j][i], nums[i][j]
+            for j in range(i+1,m):
+                nums[i][j],nums[j][i] = nums[j][i],nums[i][j]
         for row in nums:
             row.reverse()
