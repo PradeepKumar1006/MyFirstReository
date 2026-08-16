c13c98f41daf849409841c2f9699e6c531f568cd
PradeepKumar1006 <162351701+PradeepKumar1006@users.noreply.github.com>
2026-08-16 11:55:10 +0530

    Time: 1098 ms (51.29%), Space: 0B (100%) - LeetHub

--- 1280-students-and-examinations/1280-students-and-examinations.sql
+++ 1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,17 +1,5 @@
-select
-s.student_id,
-s.student_name,
-sub.subject_name,
-coalesce(count(e.subject_name),0) as attended_exams
-from students s
-cross join subjects sub
-left join examinations e
-on s.student_id = e.student_id
-and sub.subject_name = e.subject_name
-group by
-s.student_id,
-s.student_name,
-sub.subject_name
-order by 
-s.student_id,
-sub.subject_name
+select s.student_id,s.student_name,sub.subject_name,
+coalesce(count(e.subject_name)) as attended_exams
+from students s cross join subjects sub left join examinations e
+on s.student_id = e.student_id and sub.subject_name = e.subject_name
+group by 1,2,3 order by 1,2
