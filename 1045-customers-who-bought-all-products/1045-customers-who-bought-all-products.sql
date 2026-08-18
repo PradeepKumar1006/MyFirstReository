192c75c969233d25b05c0a6a2682f12a89ee3f28
PradeepKumar1006 <162351701+PradeepKumar1006@users.noreply.github.com>
2026-08-18 12:22:15 +0530

    Time: 719 ms (21.57%), Space: 0B (100%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,2 +1,2 @@
-select customer_id from customer
-group by 1 having count(distinct product_key) = (select count(product_key) from product)
+select customer_id from customer group by 1
+having count(distinct product_key) = (select count(distinct product_key) from product)
