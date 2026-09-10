023fdba61a3f42f1724fea928341eb396027fb57
PradeepKumar1006 <162351701+PradeepKumar1006@users.noreply.github.com>
2026-09-10 12:08:36 +0530

    Time: 840 ms (31.52%), Space: 0B (100%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,5 +1,13 @@
-select product_id,
-year as first_year,
-quantity,price from sales where (product_id,year) in (
-    select product_id,min(year) from sales group by product_id
+select
+    product_id,
+    year as first_year,
+    quantity,
+    price
+from sales
+where (product_id,year) in (
+    select 
+        product_id,
+        min(year)
+    from sales
+    group by product_id
 )
