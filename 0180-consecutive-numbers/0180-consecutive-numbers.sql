fd416cfd4890528f3b3d1c460c1cf9375374b6ef
PradeepKumar1006 <162351701+PradeepKumar1006@users.noreply.github.com>
2026-09-14 10:06:37 +0530

    Time: 555 ms (87.05%), Space: 0B (100%) - LeetHub

--- 0180-consecutive-numbers/0180-consecutive-numbers.sql
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -1,8 +1,6 @@
 with cte as (
-    select
-        num,
-        lead(num,1) over() as num1,
-        lead(num,2) over() as num2
-    from Logs
-) select distinct num as ConsecutiveNums from cte
-    where num = num1 and num1 = num2
+    select num,
+    lead(num,1) over(order by id) as num1,
+    lead(num,2) over(order by id) as num2
+    from logs
+)select distinct num as ConsecutiveNums  from cte where num = num1 and num = num2
