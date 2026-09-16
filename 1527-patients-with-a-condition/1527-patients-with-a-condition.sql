3e239655d3f8b5a77d59b39a29e1e492b5b46139
PradeepKumar1006 <162351701+PradeepKumar1006@users.noreply.github.com>
2026-09-16 21:28:51 +0530

    Time: 394 ms (91.77%), Space: 0B (100%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,7 +1,1 @@
-SELECT
-    patient_id,
-    patient_name,
-    conditions
-FROM Patients 
-WHERE
-    conditions like 'DIAB1%' or conditions like '% DIAB1%'
+select * from patients where conditions like 'DIAB1%' or conditions like '% DIAB1%'
